e3b0527e19e700b360c5941c0132dd7d387c924a
TheFrok <yottam@gmail.com>
2023-12-19 06:15:48 +0200

    :necktie: Started working on FileIO
    This is trial and error thing, the IO is unclear.

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -85,24 +85,24 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile p c = 
+  putStrLn ("============ " ++ p) *>
+  putStrLn c
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles = 
+  void . sequence . (<$>) (uncurry printFile)
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile p = (p, ) <$> (readFile p)
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
@@ -110,7 +110,7 @@ getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
 getFiles =
-  error "todo: Course.FileIO#getFiles"
+  sequence . (<$>) getFile
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@, @lines@, and @printFiles@.
@@ -118,7 +118,9 @@ run ::
   FilePath
   -> IO ()
 run =
-  error "todo: Course.FileIO#run"
+  printFiles =<<
+  getFiles =<<
+  (lines <*>) . readFile
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
